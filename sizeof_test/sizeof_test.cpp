a9cb2b5ae561d2151269e9027f259015ab744d03
xiabin1002 <xiabin1002@126.com>
2016-03-19 19:15:30 +0800

    Initial commit

---
+++ sizeof_test/sizeof_test.cpp
@@ -0,0 +1,36 @@
+// sizeof_test.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+#define  WFS_3Q8	(0X00000001<<1)
+#define  WFS_PT115	(0X00000001<<2)
+
+#define PR(X)	std::cout<<#X<<"=="<<X<<"\n"<<endl;
+#define CON_STR(X) std::cout<<"wfs_"<<#X<<"=="<<WFS_##X<<endl;
+
+int (*func)(int,int);
+
+int abs(int a, int b)
+{
+	return a+b;  
+}
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	int x = 1;
+	int* p = new int(0);
+	*p = 4;
+	int a = sizeof(p);
+	func = abs;
+
+	PR(func(*p, x));
+	PR(x);
+	CON_STR(3Q8);
+	CON_STR(PT115);
+
+	return 0;
+}
+
